c0407fcb22f6231bd32885ed663ee0aa7f60ebe8
ChulanguerManker <237109196+fjfernandezvalverde-debug@users.noreply.github.com>
2025-11-07 14:48:13 +0000

    Change redirect URI in kick-auth.js
    Updated redirect URI for OAuth authentication.

--- kick-auth.js
+++ kick-auth.js
@@ -2,7 +2,7 @@
 const clientId = '01K9EX32GXPW21PPGP4PB07TGW';
 
 // 🔧 OPCIONAL: Si distribuyes como ZIP, puedes usar una ruta relativa
-const redirectUri = 'https://fjfernandezvalverde-debug.github.io/chathub//callback.html';
+const redirectUri = 'https://chathub.netlify.app/callback.html';
 
 const scope = 'chat:read user:read stream:read';
 
@@ -10,3 +10,4 @@ document.getElementById('connect-btn').addEventListener('click', () => {
   const authUrl = `https://kick.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}`;
   window.open(authUrl, 'kickAuth', 'width=500,height=600');
 });
+
